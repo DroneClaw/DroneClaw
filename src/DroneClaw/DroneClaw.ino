c8e26162590a3a9aa5116c3a0ede2c23f66aeefe
Joshua Rodriguez <ewized@gmail.com>
2016-07-20 11:59:57 -0700

    Tweak things and break things

--- src/DroneClaw/DroneClaw.ino
+++ src/DroneClaw/DroneClaw.ino
@@ -3,9 +3,21 @@
 */
 #include <EventLoop.h>
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
 // Will enable debug code throught the program
-#define DEBUG
+//#define DEBUG
+
+// Escs
+#define FL_ESC 5
+#define FR_ESC 6
+#define BL_ESC 10
+#define BR_ESC 11
+
+#define CLAW 3
+
+#define BT_RX 8
+#define BT_TX 9
 
 void println(String);
 void attach();
@@ -21,33 +33,95 @@ EventLoop &scheduler = EventLoop::get();
 struct Drone {
     static const byte SERVOS = 4;
     Servo servos[SERVOS];
+    SoftwareSerial bluetooth = SoftwareSerial(BT_RX, BT_TX);
+    Servo claw = Servo();
     /** Init the drone */
     inline Drone() {
       //println("Constructing the Drone...");
       for (byte i = 0; i < SERVOS; i++) {
         servos[i] = Servo();
       }
+      bluetooth.begin(9600);
+      claw.attach(CLAW);
     }
+    static void attach();
+    static void all(const byte);
+    static void prime();
+    static void arm();
+    static void disarm();
 } drone;
 
+/** The packet class that can encode and decode data*/
+class Packet {
+  private:
+    byte _id;
+    void (*_function)(SoftwareSerial&);
+  public:
+    inline Packet(byte id, void (*function)(SoftwareSerial&)) {
+      _id = id;
+      _function = function;
+    }
+    /** This will decode the data */
+    inline void decode(SoftwareSerial &data) {
+      _function(data);
+    }
+    /** Will process the incomming packets */
+    static void process_packets();
+};
+
+/** The packets the drone knows how to handle */
+Packet packets[] = {
+  // Ping packet used to make sure there is a connection
+  Packet(0x01, [] (SoftwareSerial &data) {
+    // todo if last heart beat fails do something
+  }),
+  // Prime and arm packet, echo packet
+  Packet(0x02, [] (SoftwareSerial &data) {
+    Drone::prime();
+    Drone::arm();
+  }),
+  // Send the pos to the claw
+  Packet(0x03, [] (SoftwareSerial &data) {
+    int pos = data.readString().toInt();
+    println("Claw Position: " + String(pos));
+    drone.claw.write(data.readString().toInt());
+  }),
+  // Send data to all escs
+  Packet(0x04, [] (SoftwareSerial &data) {
+    byte pos = (byte) data.readString().toInt();
+    Drone::all(pos);
+  }),
+};
+
+/** Will process the incomming packets */
+void Packet::process_packets()  {
+  if (drone.bluetooth.available() > 0) {
+    byte packet = drone.bluetooth.read() - 'a'; // Get the packet id, treat a = 0, b = 2 ...
+    if (packet >= 0) {
+      println("Packet ID: " + String(packet));
+      packets[packet].decode(drone.bluetooth); // Lets the packet process the rest of the data
+    }
+  }
+}
+
 /** Attach all the esc to the servo instances */
-void attach() {
-  static const byte pwms[] = {5, 6, 10, 11};
+void Drone::attach() {
+  static const byte pwms[] = {FL_ESC, FR_ESC, BL_ESC, BR_ESC};
   static byte tmp = 0;
   for (byte i = 0; i < Drone::SERVOS; i++) {
-    EventLoop::get().execute([] () {
+    scheduler.execute([] () {
       println("Attaching esc " + String(tmp + 1) + " to " + String(pwms[tmp]));
       drone.servos[tmp++].attach(pwms[tmp]);
     });
   }
   // Reset tmp back to 0
-  EventLoop::get().execute([] () {
+  scheduler.execute([] () {
     tmp = 0;
   });
 }
 
 /** Send a value to all the motors */
-void all(const byte number) {
+void Drone::all(const byte number) {
   static struct _ {
     byte number;
   } _;
@@ -56,24 +130,25 @@ void all(const byte number) {
     for (byte i = 0; i < Drone::SERVOS; i++) {
       println("Executing esc " + String(i + 1) + " with " + String(_.number));
       drone.servos[i].write(_.number);
+      delay(5);
     }
   });
 }
 
 /** Prime the esc's by sending value 1 */
-void prime() {
+void Drone::prime() {
   println("Priming...");
   all(1);
 }
 
 /** Arm the motors spining at lowest speed */
-void arm() {
+void Drone::arm() {
   println("Arming...");
   all(50);
 }
 
 /** Stop all the motors */
-void disarm() {
+void Drone::disarm() {
   println("Disarming...");
   all(0);
 }
@@ -88,14 +163,9 @@ void println(String msg) {
   }
   Serial.println(msg);
   #endif
+  drone.bluetooth.println(msg);
 }
 
-void setup() {
-  println("DroneClaw");
-  console();
-  working();
-  attach();
-}
 
 /** Simple working led */
 void working() {
@@ -111,30 +181,11 @@ void working() {
   }, 2, SECONDS);
 }
 
-/** Console the manage debug commands */
-void console() {
-  #ifdef DEBUG
-  scheduler.repeat([] () {
-    if (Serial.available()) {
-      String value = Serial.readString();
-      String cmd = value.substring(0, value.indexOf(' '));
-      if (value[0] != '/') return; // not a command
-      println("Processing command: " + cmd);
-      if (cmd == "/prime") { // Prime the esc's
-        prime();
-      } else if (cmd == "/attach") { // Attach the esc's
-        attach();
-      } else if (cmd == "/arm") { // Arm the drone
-        arm();
-      } else if (cmd == "/disarm" || cmd == "//") { // Disarm the drone
-        disarm();
-      } else if (cmd == "/speed") { // Change the speed of the motors
-        int speed = value.substring(value.indexOf(' '), value.length()).toInt();
-        all(speed);
-      }
-    }
-  }, 20, MILLIS);
-  #endif
+void setup() {
+  println("DroneClaw");
+  working();
+  Drone::attach();
+  scheduler.repeat(Packet::process_packets);
 }
 
 void loop() {
